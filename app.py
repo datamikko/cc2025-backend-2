9874108c0a7e0561eb0a9f94935fce36c5166cf4
Mikko Pajula <mtpajula@gmail.com>
2025-02-26 14:12:47 +0200

    post endpoint

--- app.py
+++ app.py
@@ -1,2 +1,23 @@
-print("hello world")
+from flask import Flask, request
 
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    print('hello')
+    return "<h1>Hello! World!</h1>"
+
+
+@app.route('/sentiment', methods=['POST'])
+def get_sentiment():
+    input_data = request.json
+    print(input_data)
+
+    # here be sentiment analysis
+    # pickle
+
+    return {'input_data': input_data, 'message': 'hello!'}
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="8080", debug=False)
